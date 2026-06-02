d5ebffcf624dcfb76c1d2143b8998e536a521fde
apradoada <adrian.prado@adadevelopersacademy.org>
2026-06-02 09:47:29 -0500

    Update config approach to allow local testing

--- app/__init__.py
+++ app/__init__.py
@@ -14,29 +14,31 @@ def create_app(config=None):
     app = Flask(__name__)
     CORS(app)
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    DB_HOST = os.environ.get('DATABASE_HOST')
-    DB_PORT = int(os.environ.get('DATABASE_PORT', 5432))
-    DB_NAME = os.environ.get('DATABASE_NAME')
-    DB_USER = os.environ.get('DATABASE_USER')
-
-
-    secrets_client = boto3.client('secretsmanager')
-    secret = secrets_client.get_secret_value(SecretId=os.environ.get('DATABASE_SECRET_ARN'))
-    DB_PASSWORD = json.loads(secret['SecretString'])['password']
-
-    connection_url = URL.create(
-        drivername='postgresql+psycopg2',
-        username=DB_USER,
-        password=DB_PASSWORD,
-        host=DB_HOST,
-        port=DB_PORT,
-        database=DB_NAME
-    )
-    app.config['SQLALCHEMY_DATABASE_URI'] = connection_url
-
     if config:
         app.config.update(config)
+    
+    else:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        DB_HOST = os.environ.get('DATABASE_HOST')
+        DB_PORT = int(os.environ.get('DATABASE_PORT', 5432))
+        DB_NAME = os.environ.get('DATABASE_NAME')
+        DB_USER = os.environ.get('DATABASE_USER')
+
+
+        secrets_client = boto3.client('secretsmanager')
+        secret = secrets_client.get_secret_value(SecretId=os.environ.get('DATABASE_SECRET_ARN'))
+        DB_PASSWORD = json.loads(secret['SecretString'])['password']
+
+        connection_url = URL.create(
+            drivername='postgresql+psycopg2',
+            username=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT,
+            database=DB_NAME
+        )
+        app.config['SQLALCHEMY_DATABASE_URI'] = connection_url
+
 
     db.init_app(app)
     migrate.init_app(app, db)
